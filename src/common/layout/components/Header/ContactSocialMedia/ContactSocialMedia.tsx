a444261cfba0745a4749e76e63e71ea7cacaa12f
Juan David Marcillo Alba <jdm.a0125@gmail.com>
2023-04-21 11:30:05 -0500

    Refactor layout header to use styled component

--- src/common/layout/components/Header/ContactSocialMedia/ContactSocialMedia.tsx
+++ src/common/layout/components/Header/ContactSocialMedia/ContactSocialMedia.tsx
@@ -1,71 +1,63 @@
-import { Grid, Text, Image } from "@nextui-org/react";
+import { Grid, Text } from "@nextui-org/react";
+import {
+  ContactPhone,
+  ContactSocialMediaContainer,
+  SocialMedia,
+} from "./styled";
+import Image from "next/image";
 
 const ContactSocialMedia = () => {
   return (
-    <Grid.Container
-      direction="row"
-      justify="space-between"
-      css={{ backgroundColor: "#091D47", display: "flex", height: "60px" }}
-      alignItems="center"
-    >
-      <Grid
-        css={{
-          alignItems: "center",
-          justifyContent: "space-between",
-          display: "flex",
-          direction: "row",
-          width: "156px",
-        }}
-      >
+    <ContactSocialMediaContainer>
+      <ContactPhone>
         <Image
-          src="/assets/images/contact-icon.png"
+          width={16}
+          height={16}
           alt="contact social media"
-          containerCss={{ width: "13px", height: "12px" }}
+          src="/assets/images/contact-icon.png"
         />
         <Text size={15} color="#FAFAFA" weight="bold">
           Cali: (602) 325-8569
         </Text>
-      </Grid>
+      </ContactPhone>
 
-      <Grid
-        css={{
-          justifyContent: "space-around",
-          display: "flex",
-          direction: "row",
-          width: "130px",
-        }}
-      >
+      <SocialMedia>
         <Image
-          src="/assets/images/facebook.png"
+          width={16}
+          height={16}
           alt="facebook social media"
-          containerCss={{ width: "16px", height: "16px" }}
+          src="/assets/images/facebook.png"
         />
 
         <Image
-          src="/assets/images/twitter.png"
+          width={16}
+          height={16}
           alt="twitter social media"
-          containerCss={{ width: "16px", height: "16px" }}
+          src="/assets/images/twitter.png"
         />
 
         <Image
-          src="/assets/images/instagram.png"
+          width={16}
+          height={16}
           alt="instagram social media"
-          containerCss={{ width: "16px", height: "16px" }}
+          src="/assets/images/instagram.png"
         />
 
         <Image
-          src="/assets/images/pinterest.png"
+          width={16}
+          height={16}
           alt="pinterest social media"
-          containerCss={{ width: "16px", height: "16px" }}
+          src="/assets/images/pinterest.png"
         />
 
         <Image
-          src="/assets/images/linkedin.png"
+          width={16}
+          height={16}
           alt="linkedin social media"
-          containerCss={{ width: "16px", height: "16px" }}
+          src="/assets/images/linkedin.png"
         />
-      </Grid>
-    </Grid.Container>
+      </SocialMedia>
+    </ContactSocialMediaContainer>
   );
 };
 
---
+++ src/common/layout/components/Header/Navbar/styled.tsx
@@ -0,0 +1,81 @@
+import { Button } from "antd";
+import Image from "next/image";
+import styled from "styled-components";
+
+export const CustomImage = styled(Image)`
+  width: 100%;
+  height: auto;
+`;
+
+export const CustomControlButton = styled(Button).attrs(() => ({
+  ghost: true,
+  shape: "circle",
+}))`
+  border: none;
+  color: #fff;
+
+  & > .anticon {
+    color: #fff;
+    font-size: 24px;
+  }
+`;
+
+export const NavbarContainer = styled.section`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const SuperiorNavbar = styled.div`
+  background: linear-gradient(
+    180deg,
+    rgba(62, 121, 247, 0.65) 0%,
+    #3e79f7 53.12%,
+    #3e79f7 100%
+  );
+  width: 100%;
+  height: 60px;
+  display: flex;
+  padding: 0px 16px;
+  align-items: center;
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+export const MenuLogoContainer = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: row;
+  justify-content: space-between;
+
+  & > .menu-button {
+    margin-right: 9px;
+  }
+`;
+
+export const NavbarControlsContainer = styled.div`
+  gap: 17px;
+  display: flex;
+  align-items: center;
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+export const MenuItemsContainer = styled.nav`
+  display: flex;
+  font-size: 16px;
+  flex-direction: row;
+  justify-content: center;
+  background-color: #d4d7da;
+`;
+
+export const MenuItemButton = styled(Button).attrs(() => ({
+  ghost: true,
+}))`
+  border: none;
+  font-size: 16px;
+  font-weight: normal;
+  & > span {
+    color: #464646;
+    letter-spacing: 0.04em;
+  }
+`;
